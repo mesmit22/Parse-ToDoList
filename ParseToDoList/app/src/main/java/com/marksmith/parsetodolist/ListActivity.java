d8177ed610c047ce76971f2a1385c87b64e961a2
mesmit22 <mesmit22@gmail.com>
2015-11-09 15:30:13 -0500

    done, but not working

--- ParseToDoList/app/src/main/java/com/marksmith/parsetodolist/ListActivity.java
+++ ParseToDoList/app/src/main/java/com/marksmith/parsetodolist/ListActivity.java
@@ -1,14 +1,31 @@
 package com.marksmith.parsetodolist;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.marksmith.parsetodolist.model.Task;
+import com.marksmith.parsetodolist.model.TasksAdapter;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
     private Button btnCreate;
     private Button btnUpdate;
+    private TasksAdapter adapter;
+    private ArrayList<Task> arrayOfTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +41,49 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
-        btnUpdate = (Button)findViewById(R.id.btnUpdate);
+        arrayOfTasks = new ArrayList<>();
+        adapter = new TasksAdapter(this, arrayOfTasks);
+        ListView listView = (ListView)findViewById(R.id.lv_task);
+        listView.setAdapter(adapter);
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Task");
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+                                   @Override
+                                   public void done(List<ParseObject> taskList, ParseException e) {
+                                       if (e == null) {
+                                           for (ParseObject tasks : taskList){
+
+                                               Task task = new Task();
+
+                                               task.setTaskId(tasks.getObjectId());
+                                               task.setName(tasks.getString("name"));
+
+                                               Log.d("name", task.getName());
+                                               adapter.add(task);
+                                           }
+                                       } else {
+                                           Toast.makeText(getApplicationContext(),"Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                                       }
+                                   }
+                               });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Task task = adapter.getItem(position);
+
+                Intent intent = new Intent(getBaseContext(), UpdateActivity.class);
+                intent.putExtra("objectId", task.getTaskId());
+                startActivity(intent);
+            }
+        });
+
+                btnUpdate = (Button) findViewById(R.id.btnUpdate);
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent updateActivity = new Intent(getApplicationContext(),UpdateActivity.class);
+                Intent updateActivity = new Intent(getApplicationContext(), UpdateActivity.class);
                 startActivity(updateActivity);
             }
         });
---
+++ ParseToDoList/app/src/main/java/com/marksmith/parsetodolist/model/Task.java
@@ -0,0 +1,30 @@
+package com.marksmith.parsetodolist.model;
+
+/**
+ * Created by mark on 11/9/2015.
+ */
+public class Task {
+    public String taskId;
+    public String name;
+
+    public Task(){
+        this.taskId = "";
+        this.name = "";
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskId(){
+        return taskId;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
